308dacca8a8aab0b05a63b538a7a94aa0c09f300
hiraba <hiraba@MacBookPro.local>
2019-10-12 23:02:47 +0900

    [add] t検定における検出力とサンプルサイズの関係

--- t_test.py
+++ t_test.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
 def create_normal_sample(n, mu, sigma):
     x = np.random.normal(mu, sigma, n)
     return x
@@ -12,42 +13,59 @@ def unbiased_variance(x_i):
     v = np.sum((avg-x_i)**2)/(x_i.shape[0]-1)
     return v
 
-def exp_variance(m, n):
-    sigma = 1
-    x_m = create_normal_sample(n=m, mu=5, sigma=sigma)
-    x_n = create_normal_sample(n=n, mu=5, sigma=sigma)
+def exp_variance(m, n, m_mu, n_mu, sigma):
+    x_m = create_normal_sample(n=m, mu=m_mu, sigma=sigma)
+    x_n = create_normal_sample(n=n, mu=n_mu, sigma=sigma)
 
     #それぞれの不偏分散を計算
     v_m = unbiased_variance(x_m)
     v_n = unbiased_variance(x_n)
 
+    #それぞれのサンプル平均の差を計算
+    e = np.average(x_m) - np.average(x_n)
+
+
     #プールされた分散を計算
     v_p = ((x_m.shape[0]-1)*v_m + (x_n.shape[0]-1)*v_n)/(x_m.shape[0]+x_n.shape[0]-2)
 
     #普通に不偏分散を計算
-    x_s = np.concatenate([x_m, x_n])
-    v_s = unbiased_variance(x_s)
-    return [v_p, v_s]
+    #x_s = np.concatenate([x_m, x_n])
+    #v_s = unbiased_variance(x_s)
+    return [e, v_p]
+
+def check_prob(exp_num, m_mu, n_mu, sigma, m_size, n_size):
+
+    a = [exp_variance(m_size, n_size, m_mu, n_mu, sigma) for i in range(0, exp_num)]
+    dif_mu = np.array([v[0] for v in a])
+    dif_var = np.array([v[1] for v in a])
+
+    #t検定量の計算
+    t_val = dif_mu / np.sqrt(dif_var) * np.sqrt((m_size*n_size)) / np.sqrt(m_size+n_size)
+    
+    #有意確率の検証（t検定は近似を一切使ってないので厳密に一致）
+    significant = np.where(
+        (t_val <= scipy.stats.t.ppf(0.025, m_size+n_size-2)) | (t_val >= scipy.stats.t.ppf(0.975, m_size+n_size-2)), 
+        1,0
+    )
+    prob = np.average(significant)
+    return prob
+
+
 
 def main():
-    exp_num = 10000
-    m = 5
-    n = 5
+    #危険率の計算
+    size_list = [10, 100, 1000, 10000]
+    alpha = [check_prob(exp_num=10000, m_mu=5, n_mu=5, sigma=1, m_size=size, n_size=size) for size in size_list]
 
-    v_list = [exp_variance(m, n) for i in range(0, exp_num)]
-    v_p_list = [v[0] for v in v_list]
-    v_s_list = [v[1] for v in v_list]
-
+    #効果量を固定(0.1)した時のサンプルサイズと検出力の関係
+    size_list = [10, 100, 1000, 10000]
+    beta = [check_prob(exp_num=10000, m_mu=5, n_mu=5.1, sigma=1, m_size=size, n_size=size) for size in size_list]
+    
     plt.figure()
-    sns.distplot(v_p_list) ##自由度m+n-2->8のカイ二乗分布/3 -> 分散の理論値は0.25
-    sns.distplot(v_s_list) ##自由度m+n-1->9のカイ二乗分布/2 -> 分散の理論値は0.22
-    plt.savefig("test.png")
-
-    vp_v = np.var(v_p_list)
-    vs_v = np.var(v_s_list)
-
-    ##　参考：https://blog.rinotc.com/entry/2017/07/30/標本(不偏)分散の期待値,_分散%5B正規分布%5D
-
+    sns.pointplot(x=size_list, y=alpha, label="alpha", color='blue')
+    sns.pointplot(x=size_list, y=beta, label="beta", color='red')
+    plt.savefig("サンプルサイズと危険率_検出力.png")
+    plt.close()
     return
 
 if __name__ == "__main__":
